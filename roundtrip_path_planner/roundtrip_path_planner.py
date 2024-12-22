0e1b188033b93b3e71baacd3bc92ec6e2d6c15fa
Moritz Scheckenbach <moritz.scheckenbach@gmail.com>
2024-12-22 15:31:14 +0100

    Erweiterung Projektstruktur Anpassung roundtrip_path_planner.py

--- roundtrip_path_planner/roundtrip_path_planner.py
+++ roundtrip_path_planner/roundtrip_path_planner.py
@@ -24,28 +24,69 @@ import math
 """
 An dieser Stelle müssen für alle Algorithmen die Konfigurationen vordefiniert werden,
 sodass sie später in der GUI ausgewählt werden können und keine weiteren Parameter
-mehr nötig sind. Beispiel steht unten (zu finden im Automated_PlanerTest)
+mehr nötig sind.
+
+Kopiert aus IP-X-0-Automated_PlanerTest.ipynb
 """
 
-# basicConfig = dict()
-# basicConfig["radius"] = 3
-# basicConfig["numNodes"] = 200
-# plannerFactory["basePRM"] = [IPBasicPRM.BasicPRM, basicConfig, IPVISBasicPRM.basicPRMVisualize]
+supportedPlanners = dict()
+
+basicConfig = dict()
+basicConfig["radius"] = 3
+basicConfig["numNodes"] = 200
+supportedPlanners["basePRM"] = [IPBasicPRM.BasicPRM, basicConfig, IPVISBasicPRM.basicPRMVisualize]
+
+basicConfig2 = dict()
+basicConfig2["radius"] = 6
+basicConfig2["numNodes"] = 600
+supportedPlanners["basePRM2"] = [IPBasicPRM.BasicPRM, basicConfig2, IPVISBasicPRM.basicPRMVisualize]
+
+visbilityConfig = dict()
+visbilityConfig["ntry"] = 300
+supportedPlanners["visibilityPRM"] = [IPVisibilityPRM.VisPRM, visbilityConfig, IPVISVisibilityPRM.visibilityPRMVisualize ]
+
+# kClosestConfig = dict()
+# kClosestConfig["k"] = 7
+# kClosestConfig["numNodes"] = 300
+# supportedPlanners["kClosestPRM"] = [IPKClosestPRM.KClosestPRM, kClosestConfig, IPVISBasicPRM.basicPRMVisualize]
+
+lazyConfig = dict()
+lazyConfig["initialRoadmapSize"] = 10
+lazyConfig["updateRoadmapSize"]  = 5 
+lazyConfig["kNearest"] = 8
+supportedPlanners["lazyPRM"] = [IPLazyPRM.LazyPRM, lazyConfig, IPVISLazyPRM.lazyPRMVisualize]
+
+astarConfig = dict()
+astarConfig["heuristic"] = 'euclidean' 
+astarConfig["w"]  = 0.5
+supportedPlanners["astar"] = [IPAStar.AStar, astarConfig, IPVISAStar.aStarVisualize]
+
+# astarConfig2 = dict()
+# astarConfig2["heuristic"] = 'euclidean' 
+# astarConfig2["w"]  = 0.9
+# supportedPlanners["astar2"] = [IPAStar.AStar, astarConfig2, IPVISAStar.aStarVisualize]
+
+# rrtSimpleConfig = dict()
+# rrtSimpleConfig["numberOfGeneratedNodes"] = 100 
+# rrtSimpleConfig["testGoalAfterNumberOfNodes"]  = 10
+# supportedPlanners["simpleRRT"] = [IPRRT.RRTSimple, rrtSimpleConfig, IPVISRRT.rrtPRMVisualize]
 
 
-##############
+########################
 # Roundtrip Path Planner
-##############
+########################
 
 class Roundtrip_Path_Planner:
-    def __init__(self, startpos, targetlist, environment, algorithm, config, collChecker):
+    def __init__(self, startpos, targetlist, environment, planner, config, collChecker):
         
         # Initialize all necessary members
         self.startpos = startpos # start position in configuration space
         self.targetlist = targetlist # list of goal positions in configuration space
         self.environment = environment # reference to Environment
-        self.algorithm = algorithm # reference to algorithm
+        self.planner = planner # reference to algorithm
         self.config = config # dictionary with the needed information about the algorithms configuration options
         self.collChecker = collChecker # reference to CollisionChecker
 
-        
+    def checkPlanner(planner):
+        if not planner in supportedPlanners:
+            raise ValueError("Planner currently not supported")
