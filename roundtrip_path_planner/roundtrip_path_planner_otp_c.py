e40efa8c18faa7fbb5e1c4aa837104e9d5f320e5
Moritz Scheckenbach <moritz.scheckenbach@gmail.com>
2025-01-24 16:57:11 +0100

    Anfang Multi Query Planer

--- roundtrip_path_planner/roundtrip_path_planner_otp_c.py
+++ roundtrip_path_planner/roundtrip_path_planner_otp_c.py
@@ -167,6 +167,9 @@ class Roundtrip_Path_Planner:
 
             usedstart = self.startpos[0]
         
+        else:
+            pastgoals = goals
+        
         if isMultiQuery:
             # Multi-Query-Algorithmus: Einmalige Planung für alle Ziele
             try:
@@ -193,104 +196,151 @@ class Roundtrip_Path_Planner:
                                                        IPPerfMonitor.dataFrame()
                                                        ))
                     usedstart = pastgoals[i]
+
+                    # Visualisierung der Ergebnisse
+                    fig_local = plt.figure(figsize=(10,10))
+                    ax = fig_local.add_subplot(1,1,1)
+                    title = f"{self.plannerName} - {resultList[i].benchmark.name}"
+                    if resultList[i].solution == []:
+                        title += " (No path found!)"
+                    title += "\n Assumed complexity level " + str(resultList[i].benchmark.level)
+                    ax.set_title(title)
+                    Env_Limits = planner._collisionChecker.getEnvironmentLimits()
+                    x_Limits = Env_Limits[0]
+                    y_Limits = Env_Limits[1]
+                    ax.set_xticks(range(int(x_Limits[0]), int(x_Limits[1]) + 1))
+                    ax.set_yticks(range(int(y_Limits[0]), int(y_Limits[1]) + 1))
+                    ax.set_xlim(0, 22)
+                    ax.set_ylim(0, 22)
+                    ax.set_xlabel('X-Achse')
+                    ax.set_ylabel('Y-Achse')
+                    ax.grid(True)
+
+                    # Save Solution in whole_solution as tuple of x and y coordinates
+                    graph = resultList[i].planner.graph
+                    solution = resultList[i].solution[1:-1]  # Ignoriere den ersten und letzten Knoten (start und goal)
+                    # Füge die formatierte Version der SolutionNode hinzu
+                    for node in solution:
+                        if 'pos' in graph.nodes[node]:
+                            x = graph.nodes[node]['pos'][0]
+                            y = graph.nodes[node]['pos'][1]
+                            whole_solution.append((x, y))
+                    
+                    # Füge das Ziel der aktuellen Lösung hinzu
+                    whole_solution.append((pastgoals[i][0], pastgoals[i][1]))
+                    print(f"whole solution after adding goal: {whole_solution}")
+
+                    try:
+                        self.config[resultList[i].plannerFactoryName][2](resultList[i].planner, resultList[i].solution, ax=ax, nodeSize=100)
+                    except Exception as e:
+                        print (f"Visualizing error for planner {key}: {e}")
+                        print(f"Exception details: {e}")
+                        pass
+
+                except Exception as e:
+                    print ("PLANNING ERROR ! PLANNING ERROR ! PLANNING ERROR ")
+                    print(f"Exception details: {e}")
+                    pass
+
                     
                 except Exception as e:
                     print("PLANNING ERROR ! PLANNING ERROR ! PLANNING ERROR\nSingle Query")
                     print(f"Exception details: {e}")
                     pass
-
-        # Visualisierung der Ergebnisse
-        for i in range(len(resultList)):
-            fig_local = plt.figure(figsize=(10, 10))
-            ax = fig_local.add_subplot(1, 1, 1)
-            title = f"{self.plannerName} - {resultList[i].benchmark.name}"
-            if resultList[i].solution == []:
-                title += " (No path found!)"
-            title += "\n Assumed complexity level " + str(resultList[i].benchmark.level)
-            ax.set_title(title)
-            Env_Limits = planner._collisionChecker.getEnvironmentLimits()
-            x_Limits = Env_Limits[0]
-            y_Limits = Env_Limits[1]
-            ax.set_xticks(range(int(x_Limits[0]), int(x_Limits[1]) + 1))
-            ax.set_yticks(range(int(y_Limits[0]), int(y_Limits[1]) + 1))
-            ax.set_xlim(0, 22)
-            ax.set_ylim(0, 22)
-            ax.set_xlabel('X-Achse')
-            ax.set_ylabel('Y-Achse')
-            ax.grid(True)
-
-            # Save Solution in whole_solution as tuple of x and y coordinates
-            graph = resultList[i].planner.graph
-            solution = resultList[i].solution[1:-1]  # Ignoriere den ersten und letzten Knoten (start und goal)
-            # Füge die formatierte Version der SolutionNode hinzu
-            for node in solution:
-                if 'pos' in graph.nodes[node]:
-                    x = graph.nodes[node]['pos'][0]
-                    y = graph.nodes[node]['pos'][1]
-                    whole_solution.append((x, y))
-            
-            # Füge das Ziel der aktuellen Lösung hinzu
-            whole_solution.append((pastgoals[i][0], pastgoals[i][1]))
-            print(f"whole solution after adding goal: {whole_solution}")
-
-            try:
-                self.config[resultList[i].plannerFactoryName][2](resultList[i].planner, resultList[i].solution, ax=ax, nodeSize=100)
-            except Exception as e:
-                print (f"Visualizing error for planner {key}: {e}")
-                print(f"Exception details: {e}")
-                pass
-
-            usedstart = pastgoals[i]
-            print(f"New Usedstart: {usedstart}")
-
-        vollstaendiger_pfad = True
-
-        for i in range(len(resultList)):
-            print(f"ResultList: {resultList[i]}")
-            if resultList[i].solution == []:
-                vollstaendiger_pfad = False
-                break
-
-        if vollstaendiger_pfad:
-            try:
-                print(f"Final whole solution: {whole_solution}")
-
-                # Visualisierung des gesamten Pfades
-                # Umwandeln der Knoten-Namen in Koordinaten
-                coordinates = whole_solution
-
-                # Visualisierung des Environments
-                fig, ax = plt.subplots(figsize=(10, 10))
-                ax.set_title("Solution Path with Environment")
-
-                # Zeichnen der Hindernisse im Environment
-                planner._collisionChecker.drawObstacles(ax)
-
-                # Zeichnen des Pfads der Lösung
-                path_x = [coord[0] for coord in coordinates]
-                path_y = [coord[1] for coord in coordinates]
-                ax.plot(path_x, path_y, 'go-', label='Solution Path', zorder=2)  # 'go-' steht für grüne Punkte und Linien
-
-                # Hervorheben des Startpunkts
-                ax.plot(coordinates[0][0], coordinates[0][1], 'bo', markersize=10, label='Start', zorder=3)  # 'bo' steht für blauer Punkt
-
-                # Hervorhebn der Zwischenziele
-                for nodes in pastgoals[:-1]:
-                    ax.plot(nodes[0], nodes[1], 'C6o', markersize=10, label='intermediate Goal', zorder=3)
-
-                # Hervorheben des Zielpunkts
-                ax.plot(coordinates[-1][0], coordinates[-1][1], 'ro', markersize=10, label='Goal', zorder=3)  # 'ro' steht für roter Punkt
-
-                # Setzen der Achsenbeschriftung
+        
+        if isMultiQuery:
+            # Visualisierung der Ergebnisse
+            for i in range(len(resultList)):
+                fig_local = plt.figure(figsize=(10, 10))
+                ax = fig_local.add_subplot(1, 1, 1)
+                title = f"{self.plannerName} - {resultList[i].benchmark.name}"
+                if resultList[i].solution == []:
+                    title += " (No path found!)"
+                title += "\n Assumed complexity level " + str(resultList[i].benchmark.level)
+                ax.set_title(title)
                 Env_Limits = planner._collisionChecker.getEnvironmentLimits()
                 x_Limits = Env_Limits[0]
                 y_Limits = Env_Limits[1]
                 ax.set_xticks(range(int(x_Limits[0]), int(x_Limits[1]) + 1))
                 ax.set_yticks(range(int(y_Limits[0]), int(y_Limits[1]) + 1))
-                ax.legend()
+                ax.set_xlim(0, 22)
+                ax.set_ylim(0, 22)
+                ax.set_xlabel('X-Achse')
+                ax.set_ylabel('Y-Achse')
                 ax.grid(True)
-                plt.show()
-            except Exception as e:
-                print(f"An error occurred: {e}")
+
+            #     # Save Solution in whole_solution as tuple of x and y coordinates
+            #     graph = resultList[i].planner.graph
+            #     solution = resultList[i].solution[1:-1]  # Ignoriere den ersten und letzten Knoten (start und goal)
+            #     # Füge die formatierte Version der SolutionNode hinzu
+            #     for node in solution:
+            #         if 'pos' in graph.nodes[node]:
+            #             x = graph.nodes[node]['pos'][0]
+            #             y = graph.nodes[node]['pos'][1]
+            #             whole_solution.append((x, y))
+                
+            #     # Füge das Ziel der aktuellen Lösung hinzu
+            #     whole_solution.append((pastgoals[i][0], pastgoals[i][1]))
+            #     print(f"whole solution after adding goal: {whole_solution}")
+
+            #     try:
+            #         self.config[resultList[i].plannerFactoryName][2](resultList[i].planner, resultList[i].solution, ax=ax, nodeSize=100)
+            #     except Exception as e:
+            #         print (f"Visualizing error for planner {key}: {e}")
+            #         print(f"Exception details: {e}")
+            #         pass
+
+            #     usedstart = pastgoals[i]
+            #     print(f"New Usedstart: {usedstart}")
+
+            # vollstaendiger_pfad = True
+
+            # for i in range(len(resultList)):
+            #     print(f"ResultList: {resultList[i]}")
+            #     if resultList[i].solution == []:
+            #         vollstaendiger_pfad = False
+            #         break
+
+            # if vollstaendiger_pfad:
+            #     try:
+            #         print(f"Final whole solution: {whole_solution}")
+
+            #         # Visualisierung des gesamten Pfades
+            #         # Umwandeln der Knoten-Namen in Koordinaten
+            #         coordinates = whole_solution
+
+            #         # Visualisierung des Environments
+            #         fig, ax = plt.subplots(figsize=(10, 10))
+            #         ax.set_title("Solution Path with Environment")
+
+            #         # Zeichnen der Hindernisse im Environment
+            #         planner._collisionChecker.drawObstacles(ax)
+
+            #         # Zeichnen des Pfads der Lösung
+            #         path_x = [coord[0] for coord in coordinates]
+            #         path_y = [coord[1] for coord in coordinates]
+            #         ax.plot(path_x, path_y, 'go-', label='Solution Path', zorder=2)  # 'go-' steht für grüne Punkte und Linien
+
+            #         # Hervorheben des Startpunkts
+            #         ax.plot(coordinates[0][0], coordinates[0][1], 'bo', markersize=10, label='Start', zorder=3)  # 'bo' steht für blauer Punkt
+
+            #         # Hervorhebn der Zwischenziele
+            #         for nodes in pastgoals[:-1]:
+            #             ax.plot(nodes[0], nodes[1], 'C6o', markersize=10, label='intermediate Goal', zorder=3)
+
+            #         # Hervorheben des Zielpunkts
+            #         ax.plot(coordinates[-1][0], coordinates[-1][1], 'ro', markersize=10, label='Goal', zorder=3)  # 'ro' steht für roter Punkt
+
+            #         # Setzen der Achsenbeschriftung
+            #         Env_Limits = planner._collisionChecker.getEnvironmentLimits()
+            #         x_Limits = Env_Limits[0]
+            #         y_Limits = Env_Limits[1]
+            #         ax.set_xticks(range(int(x_Limits[0]), int(x_Limits[1]) + 1))
+            #         ax.set_yticks(range(int(y_Limits[0]), int(y_Limits[1]) + 1))
+            #         ax.legend()
+            #         ax.grid(True)
+            #         plt.show()
+            #     except Exception as e:
+            #         print(f"An error occurred: {e}")
 
         return resultList
